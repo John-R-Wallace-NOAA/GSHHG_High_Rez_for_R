8551b7f5c592e5a2d7bedf3ae92fe71c2088d7bb
John Wallace <John.Wallace@noaa.gov>
2025-01-15 16:02:06 -0800

    Update legacy code from Imap

--- R/Git.World.f.HiRez.R
+++ R/Git.World.f.HiRez.R
@@ -3,13 +3,13 @@ Git.World.f.HiRez <- function() {
    usr.warn <- options(warn = -1)
    on.exit(options(usr.warn))   
      
-   download.file("https://cdn.rawgit.com/John-R-Wallace/GSHHG_High_Rez_for_R/master/world.f.land.A.RData", "world.f.land.A.RData")
-   download.file("https://cdn.rawgit.com/John-R-Wallace/GSHHG_High_Rez_for_R/master/world.f.land.B.RData", "world.f.land.B.RData")
-   download.file("https://cdn.rawgit.com/John-R-Wallace/GSHHG_High_Rez_for_R/master/world.f.borders.lakes.rivers.RData", "world.f.borders.lakes.rivers.RData")
-
-   load("world.f.land.A.RData")
-   load("world.f.land.B.RData")
-   load("world.f.borders.lakes.rivers.RData", .GlobalEnv)
+   download.file("https://raw.githubusercontent.com/John-R-Wallace-NOAA/GSHHG_High_Rez_for_R/master/world.f.land.A.RData", "world.f.land.A.RData")
+   download.file("https://raw.githubusercontent.com/John-R-Wallace-NOAA/GSHHG_High_Rez_for_R/master/world.f.land.B.RData", "world.f.land.B.RData")
+   download.file("https://raw.githubusercontent.com/John-R-Wallace-NOAA/GSHHG_High_Rez_for_R/master/world.f.borders.lakes.rivers.RData", "world.f.borders.lakes.rivers.RData")
+   
+   base::load("world.f.land.A.RData")
+   base::load("world.f.land.B.RData")
+   base::load("world.f.borders.lakes.rivers.RData", .GlobalEnv)
 
   
    assign("world.f.land", rbind(world.f.land.A, world.f.land.B), pos = 1)
@@ -19,4 +19,3 @@ Git.World.f.HiRez <- function() {
 
    base::ls(pattern = 'world', pos = 1)
 }
-
